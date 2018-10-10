1d2148627c833237e277a26114e24cb22b9351c5
Daniel Bidulock <daniel@capitolhill.ca>
2018-10-10 14:31:04 -0600

    Simplify landing route

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,7 @@ const Agent = require('../models').Agent;
  * GET /
  */
 router.get('/', (req, res, next) => {
-  if (req.user) {
-    Agent.findById(req.user._id).then((agent) => {
-      res.render('index', { agent: agent, messages: req.flash() });
-    }).catch((error) => {
-      return res.sendStatus(501);         
-    });
-  } else {
-    res.render('index', { title: 'Accountant', agent: null, messages: req.flash() });
-  }
+  res.render('index', { agent: req.user, messages: req.flash() });
 });
 
 module.exports = router;
