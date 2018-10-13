b653dcd10e0e207fb86b0f1db2ca18c4858ee47d
Daniel Bidulock <daniel@capitolhill.ca>
2018-10-13 11:00:41 -0600

    Get note route and incipient view

---
+++ routes/note.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const models = require('../models');
+
+/**
+ * GET /note
+ */
+router.get('/', (req, res) => {
+  if (!req.isAuthenticated()) { 
+    req.flash('info', 'Login first');
+    return res.redirect('/');
+  }
+
+  res.render('note/show', { agent: req.user, messages: req.flash() });
+});
+
+module.exports = router;
